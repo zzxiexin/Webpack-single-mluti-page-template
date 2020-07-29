116683c56bbfcd1d1a6d52af91dcd09926250ca2
unknown <yuanxinxin@yiyaowang.com>
2020-07-29 16:01:15 +0800

    '单文件入口'

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let msg = `hello webpack`
+const a = () => {
+    console.log('hello es6')
+}
+console.log(msg);
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const handleCurDir = name => {
+    return path.join(__dirname, name);
+}
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+    return {
+        entry: [
+            "babel-polyfill",
+            handleCurDir('./src/index.js')
+        ],
+        output: {
+            filename: '[name].js',
+            path: handleCurDir('./dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader'
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({     // 压缩css
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            }),
+            new HtmlWebPackPlugin({            // 将打包好的文件注入html模板
+                template: "./src/index.html",
+                filename: "./index.html"
+            }),
+            new CleanWebpackPlugin(), // 清理之前的打包文件
+        ]
+    }
+}
